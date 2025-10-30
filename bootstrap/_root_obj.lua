b42df4c2541aa72cc276b6b465828250f372105f
Atoshi <95099733+atoshit@users.noreply.github.com>
2025-10-30 05:34:09 +0100

    feat(core): implement module system and file loading
    add Root object with module loading functionality and file system operations
    introduce test callback module for verification

--- bootstrap/_root_obj.lua
+++ bootstrap/_root_obj.lua
@@ -1,11 +1,41 @@
 ---@class Root
 ---@field side 'server' | 'client'
+---@field resource_name string
+---@field modules table<string, any>
+---@field LoadFile function
 local Root = {}
 local _side <const> = (IsDuplicityVersion() and 'server') or 'client'
+local _resource_name <const> = GetCurrentResourceName()
+
+--- Load a file from the resource.
+---@param path string
+local function loadFile(path)
+    if not path then
+        return warn('loadFile: path is nil')
+    end
+
+    local file_path = ("%s.lua"):format(path)
+    local file = LoadResourceFile(_resource_name, file_path)
+
+    if not file then
+        return warn('loadFile: file not found at path ^3' .. file_path .. '^7')
+    end
+
+    local _file = load(file)()
+
+    if not _file then
+        return warn('loadFile: file failed to load at path ^3' .. file_path .. '^7')
+    end
+
+    return _file
+end
 
 setmetatable(Root, {
     __index = {
-        side = _side
+        side = _side,
+        resource_name = _resource_name,
+
+        LoadFile = loadFile,
     },
 
     __newindex = function(self, key, value)
---
+++ bootstrap/modules.lua
@@ -0,0 +1,35 @@
+---@class Root.modules
+---@field loaded table<string, any>
+Root.modules = {}
+Root.modules.loaded = {}
+
+--- Load a module from the resource.
+---@param module string
+local function load(module)
+    if not module then
+        return warn('loadModule: module is nil')
+    end
+
+    if Root.modules.loaded[module] then
+        return Root.modules.loaded[module]
+    end
+
+    Root.modules.loaded[module] = Root.LoadFile(('modules/%s/%s'):format(module, Root.side))
+
+    if not Root.modules.loaded[module] then
+        return warn(('loadModule: module failed to load at path ^3modules/%s/%s.lua^7'):format(module, Root.side))
+    end
+
+    
+    return Root.modules.loaded[module], print('[^5Root^7] Module ^3' .. module .. '^7 loaded.')
+end
+
+setmetatable(Root.modules, {
+    __index = {
+        Load = load
+    },
+
+    __newindex = function(self, key, value)
+        rawset(self, key, value)
+    end
+})
